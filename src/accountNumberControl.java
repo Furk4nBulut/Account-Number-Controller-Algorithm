4a2f3631990e1ac272903933b1e44ed57fce1acc
Furkan Bulut <furkanbtng@gmail.com>
2024-11-23 18:33:15 +0300

    invalid exit added

--- src/accountNumberControl.java
+++ src/accountNumberControl.java
@@ -1,65 +1,61 @@
-
 public class accountNumberControl {
-    public static void control (String accountNumber){
-        //first 2 letter control( first letters have to less than second one!)
+    public static void control(String accountNumber) {
+        // Step 1: First 2 letter control (first letter must be less than second one!)
         char letter1, letter2;
-        letter1 = accountNumber.charAt(0); // first letter musT be less than second one
-        letter2 = accountNumber.charAt(1); // second one is greater
+        letter1 = accountNumber.charAt(0); // first letter
+        letter2 = accountNumber.charAt(1); // second letter
 
-        //letter control
         if (letter1 >= letter2) {
             System.out.println("Your account number is invalid!");
-
+            System.exit(0); // Exit the program
         }
 
+        // Step 2: Prime number control
         int primeNumber, counter = 0;
-        //to get from account number the letter for controlling
-        primeNumber = Integer.parseInt(accountNumber.substring(2, 4));
-        //primes control
+        primeNumber = Integer.parseInt(accountNumber.substring(2, 4)); // Extract the number
+
         for (int h = 1; h <= primeNumber; h++) {
             if (primeNumber % h == 0) {
-                //primitive number can divided by himself and 1
-                counter = counter + 1;
+                counter++;
             }
         }
-        if (counter != 2)//if counter is equals to  2 it is prime
 
-        {
-            System.out.println("Your account number is Invalid!");
-
+        if (counter != 2) { // If counter is not equal to 2, it's not a prime number
+            System.out.println("Your account number is invalid!");
+            System.exit(0); // Exit the program
         }
-        //Step 3
-        String lastNumbers = accountNumber.substring(4, accountNumber.length());
-        int sum1 = 0, sum2 = 0, temp3 = 0,total =0;
-        String deneme = "";
 
-        //there is a loop to control is it version of multiplication greater than 10
-        for (int f = 0; f < 12; f = f + 2) {
-            if (Integer.parseInt(String.valueOf(lastNumbers.charAt(f))) * 2 < 10) //check is it greate than 10
-            {
-                sum1 = sum1 + Integer.parseInt(String.valueOf(lastNumbers.charAt(f))) * 2; //sum if it is not greater than 10
+        // Step 3: Checksum control
+        String lastNumbers = accountNumber.substring(4);
+        int sum1 = 0, sum2 = 0, temp3 = 0, total = 0;
+        String tempStr;
+
+        // Loop to control whether the double of each number is greater than 10
+        for (int f = 0; f < 12; f += 2) {
+            int currentNum = Integer.parseInt(String.valueOf(lastNumbers.charAt(f)));
+
+            if (currentNum * 2 < 10) {
+                sum1 += currentNum * 2; // Add if less than 10
             } else {
-                temp3 = Integer.parseInt(String.valueOf(lastNumbers.charAt(f))) * 2; // if it is greater than 10 smash and collect
-                deneme = String.valueOf(temp3); // for taking   2 digit
-                sum1 = sum1 + Integer.parseInt(String.valueOf(deneme.charAt(0))) + Integer.parseInt(String.valueOf(deneme.charAt(1))); //adding 2 digit
+                temp3 = currentNum * 2; // If greater than 10, split and sum the digits
+                tempStr = String.valueOf(temp3);
+                sum1 += Integer.parseInt(String.valueOf(tempStr.charAt(0))) +
+                        Integer.parseInt(String.valueOf(tempStr.charAt(1)));
             }
         }
-        //sum loop for other numbers
-        for (int g = 1; g < 12; g= g + 2) {
 
-            sum2 = sum2 + Integer.parseInt(String.valueOf(lastNumbers.charAt(g)));
-
+        // Sum loop for other digits
+        for (int g = 1; g < 12; g += 2) {
+            sum2 += Integer.parseInt(String.valueOf(lastNumbers.charAt(g)));
         }
-        //sum
+
+        // Final checksum validation
         total = sum1 + sum2;
-        if(total%10 == 0){
+        if (total % 10 == 0) {
             System.out.println("Account number is valid!");
-        }
-        else {
-            System.out.println("Account number is invalid");
-
+        } else {
+            System.out.println("Account number is invalid!");
+            System.exit(0); // Exit the program
         }
     }
-
 }
-
